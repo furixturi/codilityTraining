d13ae98292e0294736376bd5d7999917dd42ecf7
furixturi <alabebop@gmail.com>
2017-08-15 22:20:48 +0900

    add 06 sorting 03 triangle

---
+++ 06-03-Triangle.py
@@ -0,0 +1,54 @@
+# 06 Sort - 03 Triangle
+'''
+Determine whether a triangle can be built from a given set of edges.
+Task description
+A zero-indexed array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+
+A[P] + A[Q] > A[R],
+A[Q] + A[R] > A[P],
+A[R] + A[P] > A[Q].
+For example, consider array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+Triplet (0, 2, 4) is triangular.
+
+Write a function:
+
+def solution(A)
+
+that, given a zero-indexed array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+
+For example, given array A such that:
+
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+the function should return 1, as explained above. Given array A such that:
+
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+the function should return 0.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(N*log(N));
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+Elements of input arrays can be modified.
+'''
+
+def solution(A):
+    # write your code in Python 2.7
+    if len(A) < 3:
+        return 0
+    A.sort()
+    i = 0
+    while (i < len(A)-2):
+        if(A[i] + A[i+1] > A[i+2]):
+            return 1
+        else:
+            i += 1
+    return 0
